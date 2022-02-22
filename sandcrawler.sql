8382bcd83eaf2e30664d2586e48c53d429d29e03
Matt McElwee <mmcelwee@dmsi.com>
2022-02-22 02:42:38 -0600

    Swapped out sql script for db dump

--- sandcrawler.sql
+++ sandcrawler.sql
@@ -1,39 +1,122 @@
--- Items table
-CREATE TABLE IF NOT EXISTS items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    msrp INT DEFAULT 0 NOT NULL,
-    manufacturer VARCHAR(255),
-    description BLOB,
-    overview BLOB,
-    warranty BLOB,
-    image VARCHAR(255) NOT NULL
-);
+-- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: sandcrawler
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
--- Item specs table
-CREATE TABLE IF NOT EXISTS item_specs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    item_id INT NOT NULL,
-    label VARCHAR(255) NOT NULL,
-    spec_value VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- ALTER TABLE item_specs
---    ADD CONSTRAINT item_specs__items
---    FOREIGN KEY (item_id)
---    REFERENCES items(id);
+--
+-- Current Database: `sandcrawler`
+--
 
--- Item pricing table
-CREATE TABLE IF NOT EXISTS item_pricing_books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    item_id INT NOT NULL,
-    amount INT NOT NULL,
-    sale_name VARCHAR(100),
-    valid_from VARCHAR(25),
-    valid_to VARCHAR(25)
-);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sandcrawler` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-ALTER TABLE item_pricing_books
-    ADD CONSTRAINT item_pricing_books__items
-    FOREIGN KEY (item_id)
-    REFERENCES items(id);
+USE `sandcrawler`;
+
+--
+-- Table structure for table `item_pricing_books`
+--
+
+DROP TABLE IF EXISTS `item_pricing_books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_pricing_books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_id` int NOT NULL,
+  `amount` int NOT NULL,
+  `sale_name` varchar(100) DEFAULT NULL,
+  `valid_from` varchar(25) DEFAULT NULL,
+  `valid_to` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_pricing_books__items` (`item_id`),
+  CONSTRAINT `item_pricing_books__items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_pricing_books`
+--
+
+LOCK TABLES `item_pricing_books` WRITE;
+/*!40000 ALTER TABLE `item_pricing_books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_pricing_books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_specs`
+--
+
+DROP TABLE IF EXISTS `item_specs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_specs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_id` int NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `spec_value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_specs__items` (`item_id`),
+  CONSTRAINT `item_specs__items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_specs`
+--
+
+LOCK TABLES `item_specs` WRITE;
+/*!40000 ALTER TABLE `item_specs` DISABLE KEYS */;
+INSERT INTO `item_specs` VALUES (1,1,'Height','1.7 meters'),(2,1,'Photoreceptors','2 (human range)'),(3,1,'Auditory receptors','2 (human range)');
+/*!40000 ALTER TABLE `item_specs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `msrp` int NOT NULL DEFAULT '0',
+  `manufacturer` varchar(255) DEFAULT NULL,
+  `description` blob,
+  `overview` blob,
+  `warranty` blob,
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'3PO-series protocol droid',3000,'Cybot Galactica','','','','https://static.wikia.nocookie.net/starwars/images/8/85/PrissyAndPrissier-ST.jpg/revision/latest/scale-to-width-down/1000?cb=20090118041926');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-22  8:27:08
